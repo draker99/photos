ca4b9dbb9dbec180e0506a2b51300307b5fb56c2
draker99 <c.pipp@a1.net>
2018-02-18 06:34:25 +0100

    added several list functions

--- Photos/Views/PhotosPage.xaml.cs
+++ Photos/Views/PhotosPage.xaml.cs
@@ -1,20 +1,59 @@
-﻿using Microsoft.WindowsAzure.MobileServices;
+﻿using System.Diagnostics;
+using System.IO;
+using Microsoft.WindowsAzure.MobileServices;
 using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Photos
 {
     public partial class PhotosPage : ContentPage
     {
+        public ObservableCollection<UserPicture> pic { get; set; }
         public PhotosPage()
         {
             InitializeComponent();
 
-            this.Insert();
+            UserPicture p = new UserPicture();
+            pic = new ObservableCollection<UserPicture>();
 
+
+            // Handling my events
+            b_addimage.Clicked += (sender, e) =>
+            {
+                this.GetPhoto();
+            };
+                      
+
+            // Getting data back from native system
+            MessagingCenter.Subscribe<byte[]>(this, "ImageSelected", (args) =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    //Set the source of the image view with the byte array
+                   
+                    p.Picture = ImageSource.FromStream(() => new MemoryStream((byte[])args));
+                    i_picture.Source = p.Picture;
+                    pic.Add(new UserPicture(p.Picture));
+                    lstView.ItemsSource = pic;
+                    Debug.WriteLine("I am the Message Center" +  pic.Count);
+                });
+            });
          
         }
 
-        public async void Insert()   {
+         void GetPhoto()   {
+         
+            Device.BeginInvokeOnMainThread(() =>
+             {
+
+                DependencyService.Get<CameraInterface>().BringUpPhotoGallery();
+            
+              });
+
+        }
+
+    /*    public async void Insert()   {
 
             AzureConnection conn = new AzureConnection();
 
@@ -22,5 +61,6 @@ namespace Photos
             await conn.client.GetTable<UserPicture>().InsertAsync(picture);
             
         }
+        */
     }
 }
